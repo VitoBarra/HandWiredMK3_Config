fd406ce7e8115ecfba1d7958bf29792939fefcb6
Vito Barra <52669351+VitoBarra@users.noreply.github.com>
2025-03-17 02:18:58 +0100

    Update keymap.c

--- keymaps/vb/keymap.c
+++ keymaps/vb/keymap.c
@@ -61,15 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		          KC_TRNS,  XXXXXXX,  KC_TRNS,  KC_TRNS,   /**/ KC_TRNS,  KC_TRNS,  KC_TRNS,  XXXXXXX
   ),
   [Games] = LAYOUT_ortho_10x4(
-		KC_LALT,   KC_Q,      KC_W,            KC_E,      KC_R,    /**/  KC_T,    KC_Y,    KC_U,   KC_I,   KC_O,  
-        KC_LCTL,   KC_A,      KC_S,            KC_D,      KC_F,    /**/  KC_G,    KC_H,    KC_J,   KC_K,   KC_L,
-		KC_LSFT,   KC_Z,      KC_X,            KC_C,      KC_V,    /**/  KC_B,    KC_N,    KC_M,   KC_P,   XXXXXXX,
-		           KC_ESC,    MO(GamesExtra),  KC_TAB,   KC_SPC,   /**/  KC_BSPC, KC_DEL, KC_ENT, TD(TD_BASE)),
+		KC_LALT,   KC_Q,      KC_W,            KC_E,      KC_R,  /**/  KC_T,    KC_Y,    KC_U,   KC_I,   KC_O,  
+                KC_LCTL,   KC_A,      KC_S,            KC_D,      KC_F,  /**/  KC_G,    KC_H,    KC_J,   KC_K,   KC_L,
+		KC_LSFT,   KC_Z,      KC_X,            KC_C,      KC_V,  /**/  KC_B,    KC_N,    KC_M,   KC_P,   XXXXXXX,
+		           KC_ESC,    MO(GamesExtra),  KC_TAB,   KC_SPC, /**/  KC_BSPC, KC_DEL, KC_ENT, TD(TD_BASE)),
   [GamesExtra] = LAYOUT_ortho_10x4(
-		KC_1,      KC_2,      KC_3,       KC_4,     KC_5,    /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  
-        KC_6,      KC_7,      KC_8,       KC_9,     KC_0,    /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-		KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT,  XXXXXXX,    /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-		           XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,    /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX)
+		KC_1,      KC_2,      KC_3,       KC_4,     KC_5,  /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  
+                KC_6,      KC_7,      KC_8,       KC_9,     KC_0,  /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+		KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT,  XXXXXXX,  /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+		           XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  /**/  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX)
 };
 
 // clang-format on
